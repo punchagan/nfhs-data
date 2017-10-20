0b25f61ef00d1b2ea557a009289c13e9a5f8da7a
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-10-20 20:20:19 +0530

    Show correlation heatmaps

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def get_category(data, indicator):
     categories = data[data['indicator'] == indicator]['indicator_category']
     return categories.unique()[0]
 
+
 i_c_map = {
     indicator: get_category(nfhs_state_wise, indicator)
     for indicator in indicators
@@ -49,59 +50,53 @@ title = 'NFHS-4 EDA'
 app = dash.Dash()
 app.title = title
 
-graphs = ['Single Indicator', 'Correlations']
 
-app.layout = html.Div(children=[
-    html.H1(children=title),
-    dcc.Dropdown(
-        id='indicator-dropdown',
-        options=[
-            {
-                'label': '%s :: %s' % (i_c_map[indicator], indicator),
-                'value': indicator
-            }
-            for indicator in indicators
+def get_indicator_values(data, indicator):
+    X = data[data['indicator'] == indicator]
+    X_ = X[['rural', 'urban', 'total']]
+    X_.index = X['state']
+    return X_[:-1]  # FIXME: WB data appears twice
+
+
+def compute_indicator_correlations(data, indicators):
+    rural, urban, total = {}, {}, {}
+    I = indicators
+    for i, indicator in enumerate(I):
+        print(i)
+        X = get_indicator_values(data, indicator)
+        correlations = [
+            X.corrwith(get_indicator_values(data, other))
+            for other in I
+        ]
+        correlations = pandas.DataFrame(correlations, index=I)
+        rural[indicator] = correlations['rural']
+        urban[indicator] = correlations['urban']
+        total[indicator] = correlations['total']
+
+    rural = pandas.DataFrame(rural).sort_index()
+    urban = pandas.DataFrame(urban).sort_index()
+    total = pandas.DataFrame(total).sort_index()
+    return rural, urban, total
+
+
+RURAL, URBAN, TOTAL = compute_indicator_correlations(
+    nfhs_state_wise, indicators
+)
+
+
+def correlation_heatmap(data):
+    figure = {
+        'data': [
+            go.Heatmap(z=data.values,
+                       x=data.columns,
+                       y=data.index)
         ],
-        value=indicators[0],
-        multi=True,
-    ),
-    dcc.Graph(id='nfhs-graph')
-])
-
-app.css.append_css({
-    'external_url': "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
-
-
-def category_indicators(data, category=None):
-    category_data = (
-        data[data['indicator_category'] == category]
-        if category is not None else data
-
-    )
-    return sorted(set(category_data['indicator']))
-
-
-@app.callback(Output('nfhs-graph', 'figure'),
-              [Input('indicator-dropdown', 'value')])
-def update_graph(indicators):
-    if isinstance(indicators, str):
-        indicator = indicators
-        data = nfhs_state_wise[nfhs_state_wise['indicator'] == indicator]
-        figure = single_scatter(data)
-
-    elif len(indicators) == 1:
-        indicator = indicators[0]
-        data = nfhs_state_wise[nfhs_state_wise['indicator'] == indicator]
-        figure = single_scatter(data)
-
-    elif len(indicators) >= 2:
-        x, y = indicators[:2]
-        figure = correlation_scatter(nfhs_state_wise, x, y)
-
-    else:
-        figure = None
-
+        'layout': {
+            'height': 800,
+            'width': 900,
+            'hovermode': 'closest',
+        }
+    }
     return figure
 
 
@@ -164,6 +159,63 @@ def correlation_scatter(data, indicator_x, indicator_y):
             },
         }
     }
+    return figure
+
+
+app.layout = html.Div(children=[
+    html.H1(children=title),
+    dcc.Dropdown(
+        id='indicator-dropdown',
+        options=[
+            {
+                'label': '%s :: %s' % (i_c_map[indicator], indicator),
+                'value': indicator
+            }
+            for indicator in indicators
+        ],
+        value=indicators[0],
+        multi=True,
+    ),
+    dcc.Graph(id='nfhs-graph'),
+    dcc.Graph(id='nfhs-correlations-rural', figure=correlation_heatmap(RURAL)),
+    dcc.Graph(id='nfhs-correlations-urban', figure=correlation_heatmap(URBAN)),
+    dcc.Graph(id='nfhs-correlations-total', figure=correlation_heatmap(TOTAL)),
+])
+
+app.css.append_css({
+    'external_url': "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
+
+
+def category_indicators(data, category=None):
+    category_data = (
+        data[data['indicator_category'] == category]
+        if category is not None else data
+
+    )
+    return sorted(set(category_data['indicator']))
+
+
+@app.callback(Output('nfhs-graph', 'figure'),
+              [Input('indicator-dropdown', 'value')])
+def update_graph(indicators):
+    if isinstance(indicators, str):
+        indicator = indicators
+        data = nfhs_state_wise[nfhs_state_wise['indicator'] == indicator]
+        figure = single_scatter(data)
+
+    elif len(indicators) == 1:
+        indicator = indicators[0]
+        data = nfhs_state_wise[nfhs_state_wise['indicator'] == indicator]
+        figure = single_scatter(data)
+
+    elif len(indicators) >= 2:
+        x, y = indicators[:2]
+        figure = correlation_scatter(nfhs_state_wise, x, y)
+
+    else:
+        figure = None
+
     return figure
 
 
